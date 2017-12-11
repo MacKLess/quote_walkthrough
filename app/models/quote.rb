04f93fae0bd04997b1274ac35dfffb9e5e972928
LDMacKrell <ldmackrell@gmail.com>
2017-12-11 11:49:26 -0800

    Create app.

---
+++ app/models/quote.rb
@@ -0,0 +1,4 @@
+class Quote < ActiveRecord::Base
+  validates :author, :content, :presence => true
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory(:quote) do
+    author("Mae West")
+    content("Everything Chuck Norris can do, Bruce Lee can do better.")
+  end
+end
